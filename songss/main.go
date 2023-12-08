6ba88dc4d08f94231caf7aca9ac86ba08826dd5e
Shakezidin <sinuzidin@gmail.com>
2023-12-08 12:44:31 +0530

    templates added

--- songss/main.go
+++ songss/main.go
@@ -2,35 +2,70 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	port      = 8080
+	songsDir  = "bachgavotteshort"
+	videoDir  = "footballmp4"
+	mediaHTML = "templates/player.html"
+)
+
+// MediaData represents data to be passed to the HTML template.
+type MediaData struct {
+	VideoURL string
+	AudioURL string
+}
+
 func main() {
 	r := gin.Default()
 
-	const port = 8080
-	const songsDir = "bachgavotteshort"
-	const videoDir = "footballmp4"
-
+	// Middleware for CORS support
 	r.Use(func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "*")
 		c.Next()
 	})
 
-	r.GET("/video/:playlist.m3u8", func(c *gin.Context) {
-		filename := c.Param("playlist.m3u8")
-		c.File(fmt.Sprintf("%s/%s", videoDir, filename))
+	// Route for serving media player page
+	r.GET("/play/:filename", func(c *gin.Context) {
+		filename := c.Param("filename")
+		videoURL := fmt.Sprintf("/video/%s", filename)
+		audioURL := fmt.Sprintf("/audio/%s", filename)
+
+		data := MediaData{
+			VideoURL: videoURL,
+			AudioURL: audioURL,
+		}
+
+		render(c, mediaHTML, data)
 	})
 
-	r.GET("/audio/:outputlist.m3u8", func(c *gin.Context) {
-		filename := c.Param("outputlist.m3u8")
-		c.File(fmt.Sprintf("%s/%s", songsDir, filename))
-	})
+	// Route for serving video and audio files
+	r.Static("/video", videoDir)
+	r.Static("/audio", songsDir)
 
 	fmt.Printf("Starting server on %v\n", port)
 
+	// Run the server
 	if err := r.Run(fmt.Sprintf(":%v", port)); err != nil {
 		fmt.Printf("Error starting server: %v\n", err)
 	}
 }
+
+func render(c *gin.Context, templatePath string, data interface{}) {
+	tmpl, err := template.ParseFiles(templatePath)
+	if err != nil {
+		c.String(500, "Internal Server Error")
+		return
+	}
+
+	err = tmpl.Execute(c.Writer, data)
+	if err != nil {
+		c.String(500, "Internal Server Error")
+		return
+	}
+}
+
